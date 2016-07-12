1bff8d758fb84e148e27746730135c5791e04a47
Nathan Talbot <Nathan@Nathans-MacBook-Air.local>
2016-07-12 22:43:14 +0800

    Added selection css

--- js/src/modules/utils.js
+++ js/src/modules/utils.js
@@ -5,29 +5,6 @@
 // Required
 // .....
 
-/*var setCookie = function(name, val, days){
-    var date = new Date();
-    date.setTime(date.getTime()+(days*24*60*60*1000));
-    var expires = "; expires="+date.toGMTString();
-    document.cookie = name + "=" + val + expires + ";";
-};
-
-var getCookie = function(name){
-
-    var cookie = name + "=";
-    var array = document.cookie.split(';');
-
-    $.each(array, function(i){
-        var c = array[i];
-         while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(cookie) == 0) return c.substring(cookie.length, c.length);
-    });
-
-    return '';
-
-};
-
 module.exports = {
-    setCookie: setCookie,
-    getCookie: getCookie,
-}*/
+
+}
